fd6648b5a252a50efc970c6f3f4e8d3216944cbb
John Pan <john_pan@symantec.com>
2020-01-06 21:03:14 -0500

    fix couldbuild.yaml

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -8,7 +8,7 @@ SERV_IP=$(grep external_ip $TF_STATE | awk '{ print $3}')
 echo "$SERV_IP"
 [ "$SERV_IP" == "" ] && exit 1
 
-RESPONSE=$(curl "http://$SERV_IP/" --connect-timeout 5)
+RESPONSE=$(curl "http://$SERV_IP/" --connect-timeout 15)
 echo "$RESPONSE"
 
 [ "$RESPONSE" == "<html><body><h1>Environment: $BRANCH_NAME</h1></body></html>" ] || exit 1
